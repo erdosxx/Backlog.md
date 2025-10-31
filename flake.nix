64743e5c432d38c697c9baaa059a24c9e9f2f3e8
Norel <norel.evoagile@gmail.com>
2025-10-31 11:48:58 +0900

    Update name for overlay.

--- flake.nix
+++ flake.nix
@@ -18,21 +18,19 @@
           inherit system;
           overlays = [
             (final: prev: {
-              bun = prev.bun.overrideAttrs (finalAttrs: previousAttrs:
+              bun = prev.bun.overrideAttrs (finalAttrs: oldAttrs:
                 let bunVersion = "1.2.23";
                 in {
                   src =
                     finalAttrs.passthru.sources.${prev.stdenvNoCC.hostPlatform.system} or (throw
                       "Unsupported system: ${prev.stdenvNoCC.hostPlatform.system}");
 
-                  passthru = previousAttrs.passthru // {
-                    sources = previousAttrs.passthru.sources // {
+                  passthru = oldAttrs.passthru // {
+                    sources = oldAttrs.passthru.sources // {
                       "x86_64-linux" = prev.fetchurl {
                         url =
                           "https://github.com/oven-sh/bun/releases/download/bun-v${bunVersion}/bun-linux-x64-baseline.zip";
                         sha256 = "017f89e19e1b40aa4c11a7cf671d3990cb51cc12288a43473238a019a8cafffc";
-                        # hash =
-                        #   "sha256-oPlaeSdMBsJSzaq/HQ6HjhXQ0wZ5v2dS4DJuwUEwIyM=";
                       };
                     };
                   };
